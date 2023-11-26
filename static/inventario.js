6a774052745bd2db670dbd738ef7b253333598a0
whosnono <111985692+whosnono@users.noreply.github.com>
2023-11-25 17:58:51 -0700

    botones

--- static/inventario.js
+++ static/inventario.js
@@ -12,9 +12,22 @@ function cerrarModalAñadirArticulo() {
   }
 
 function abrirModalActualizarArticulo(){
+  document.getElementById("modalActualizarArticulo").style.display = "block";
+  $.datetimepicker.setLocale('es');
+$(".datetimepicker").datetimepicker({
+  format: "Y-m-d H:i",  // Formato de fecha y hora
+  step: 15,             // Incremento de minutos
+});
+}
 
+function cerrarModalActualizarArticulo() {
+  document.getElementById("modalActualizarArticulo").style.display = "none";
 }
 
 function abrirModalEliminarArticulo(){
-    
+  document.getElementById("modalEliminarArticulo").style.display = "block";
+}
+
+function cerrarModalEliminarArticulo() {
+  document.getElementById("modalEliminarArticulo").style.display = "none";
 }
