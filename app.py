7c0caeeb998b49260a4d4faf5103f578a1883249
whosnono <111985692+whosnono@users.noreply.github.com>
2023-11-25 19:07:55 -0700

    segundo intento

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def inventario():
             with connection.cursor() as cursor:
                 sql = "UPDATE PIEZA SET Nombre=%s, CantidadEnStock=%s, FechaAdquisicion=%s, PrecioCompra=%s, PrecioVenta=%s, IDProveedor=%s WHERE IDPieza=%s"
                 cursor.execute(sql, (Nombre, CantidadEnStock, 
-                                     FechaAdquisicion, PrecioCompra, PrecioVenta, IDProveedor))
+                                     FechaAdquisicion, PrecioCompra, PrecioVenta, IDProveedor, IDPieza))
 
             connection.commit()
             response = {'status': 'success',
